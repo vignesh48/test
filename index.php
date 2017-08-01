3cf5320e1a656d14046282463c95438bb8b25e4f
vignesh48 <30625170+vignesh48@users.noreply.github.com>
2017-08-01 17:59:11 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+    <html>
+    <head>
+        <title>Task1</title>
+    </head>
+    <body>
+        <div class="header"></div>
+        <div class=session><p><pre>
+            <strong>Avul Pakir Jainulabdeen Abdul Kalam</strong> was the 11th President of India.
+            A career scientist turned politician, Kalam was born and raised in Rameswaram.
+            and studied physics and aerospace engineering. 
+            <strong>Born:</strong>October 15, 1931, Rameswaram
+            <strong>Died:</strong>July 27, 2015, Shillong
+            <strong>Full name:</strong> Avul Pakir Jainulabdeen Abdul Kalam
+            <strong>Awards:</strong>Bharat Ratna, Padma Bhushan, Padma Vibhushan.
+        </pre></p></div>
+        <div class=footer></div>
+    </body>
+</html> 
